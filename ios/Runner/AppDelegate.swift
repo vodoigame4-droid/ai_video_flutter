25cbb478dc36aa55859398a5124c2ee55810c66a
Phạm Quang Linh <76081683+quanglinh301098@users.noreply.github.com>
2026-06-05 09:35:59 +0700

    feat: Implement local notification handling and APNs configuration for iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    application.registerForRemoteNotifications()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
@@ -14,3 +15,4 @@ import UIKit
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
+
